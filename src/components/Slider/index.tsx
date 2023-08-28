2c7638bbd98d947bd360c5a803fbe25181d14bed
Alexander Erkhov <tyrantbud@yandex.ru>
2023-08-28 14:39:51 +0300

    Fix index page and menu layout

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -15,53 +15,54 @@ import { Navigation } from 'swiper';
 
 const Slider = () => {
     return (
-            <div className="slider">
-                <Swiper
-                    modules={[Navigation]}
-                    direction="horizontal"
-                    slidesPerView={1}
-                    navigation={{
-                        nextEl: '.slider__next',
-                        prevEl: '.slider__prev',
-                    }}
-                >
-                    <div className="slider__wrap">
-                        <SwiperSlide>
-                            <div className="slider__content">
-                                <div className="slider__left">
-                                    <div className="slider__desc">Не черномор</div>
-                                    <h5 className="slider__title">Дан старт корпоративной эстафете в честь юбилея ПАО «Транснефть»</h5>
-                                </div>
-                                <div className="slider__right">
-                                    <img src={img} alt="" className="slider__img" />
-                                </div>
+        <div className="slider">
+            <Swiper
+                modules={[Navigation]}
+                direction="horizontal"
+                slidesPerView={1}
+                spaceBetween={50}
+                navigation={{
+                    nextEl: '.slider__next',
+                    prevEl: '.slider__prev',
+                }}
+            >
+                <div className="slider__wrap">
+                    <SwiperSlide>
+                        <div className="slider__content">
+                            <div className="slider__left">
+                                <div className="slider__desc">Не черномор</div>
+                                <h5 className="slider__title">Дан старт корпоративной эстафете в честь юбилея ПАО «Транснефть»</h5>
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slider__content">
-                                <div className="slider__left">
-                                    <div className="slider__desc">Не черномор</div>
-                                    <h5 className="slider__title">Дан старт корпоративной эстафете в честь юбилея ПАО «Транснефть»</h5>
-                                </div>
-                                <div className="slider__right">
-                                    <img src={img} alt="" className="slider__img" />
-                                </div>
+                            <div className="slider__right">
+                                <img src={img} alt="" className="slider__img" />
                             </div>
-                        </SwiperSlide>
-                    </div>
-                </Swiper>
-                <div className="slider__counter">
-                    <div className="swiper-button-prev slider__prev">
-                        <ChevronLeftIcon />
-                    </div>
-                    <div className="slider__current">1</div>
-                    <div>/</div>
-                    <div className="slider__total">0</div>
-                    <div className="swiper-button-next slider__next">
-                        <ChevronRightIcon />
-                    </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="slider__content">
+                            <div className="slider__left">
+                                <div className="slider__desc">Не черномор</div>
+                                <h5 className="slider__title">Дан старт корпоративной эстафете в честь юбилея ПАО «Транснефть»</h5>
+                            </div>
+                            <div className="slider__right">
+                                <img src={img} alt="" className="slider__img" />
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                </div>
+            </Swiper>
+            <div className="slider__counter">
+                <div className="swiper-button-prev slider__prev">
+                    <ChevronLeftIcon />
+                </div>
+                <div className="slider__current">1</div>
+                <div>/</div>
+                <div className="slider__total">0</div>
+                <div className="swiper-button-next slider__next">
+                    <ChevronRightIcon />
                 </div>
             </div>
+        </div>
     );
 };
 
