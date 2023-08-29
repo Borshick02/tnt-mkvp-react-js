64ec550fe8a9f23c5352f1daade0db51d83f2826
Denis Demin <77795430+prostodennik@users.noreply.github.com>
2023-08-29 17:48:20 +0300

    fix: home page swipers arrow

--- src/components/SwiperVideo/index.tsx
+++ src/components/SwiperVideo/index.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
+// import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 import './index.scss';
 
